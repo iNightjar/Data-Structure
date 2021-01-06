6220052959434b87cdf8b20d59acc9558cef5fc4
iam-medhat <60796459+iam-medhat@users.noreply.github.com>
2021-01-06 12:55:00 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,50 @@
+package datastructure;
+
+/*
+public class Main { // recursion algorithms
+    public static void main(String[] args) {
+        System.out.println("the result is: " + recursion.fact(5,1));
+        System.out.println("the result is: " + recursion.sum(5,0));
+        System.out.println("the resutl is : " + recursion.mul(4,1));
+       System.out.println("the result is :  " + recursion.power(3,3,1));
+    }
+}*/
+
+/* // unsortedarray .. insert , delete and search
+public class Main
+{
+    public static void main(String[] args) {
+        unorderarray a = new unorderarray(7);
+        a.insert(12);
+        a.insert(2);
+        a.insert(3);
+        a.insert(4);
+        a.insert(37);
+        a.insert(10);
+
+        a.deletefirstmatch(12);
+        a.displaydata();
+    }
+} */
+ /*  // sortedarray .. insert and delete .. binary search
+public class Main
+{
+    public static void main(String[]args)
+    {
+        sortedarray a = new sortedarray(10);
+        a.insert(1);
+        a.insert(27);
+        a.insert(3);
+        a.insert(25);
+        a.insert(4);
+        a.insert(87);
+        a.insert(19);
+        a.insert(23);
+        a.insert(22);
+        a.displaydata();
+        a.deletedvalue(23);
+        a.displaydata();
+        System.out.println(a.binarysearch(19));
+        a.displaydata();
+    }
+} */
