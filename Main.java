5fa59451285cecee9eb5ab593af80094e3000028
iam-medhat <60796459+iam-medhat@users.noreply.github.com>
2021-01-06 13:03:48 +0200

    second commit

--- Main.java
+++ Main.java
@@ -1,18 +1,44 @@
 package datastructure;
 
+public class Main{
+    public static void main(String [] args) {
+    
+    }
+/* public class Main{ // StackUsingArray
+    public static void main(String[] args) {
+        String s = "iNightjar"; // working with strings
+        StackUsingArray str = new StackUsingArray(s.length());
+        for (int i = 0; i < s.length(); i++)
+        {
+            int ch = s.charAt(i); // char that is equal to the ASCII value of string
+            str.push(ch);
+        }
+        while(!str.isEmpty())
+        {
+            char ch = (char)str.pop();
+            System.out.print(ch);
+        } /*
+       /* StackUsingArray a= new StackUsingArray(3);
+        a.push(5);
+        a.push(12);
+        System.out.println("the Top of The Stack is: " + a.peek());
+        System.out.println("the Top of The Stack After pop is : " + a.pop()); */
+    }
+}
+
 /*
 public class Main { // recursion algorithms
     public static void main(String[] args) {
         System.out.println("the result is: " + recursion.fact(5,1));
         System.out.println("the result is: " + recursion.sum(5,0));
-        System.out.println("the resutl is : " + recursion.mul(4,1));
+        System.out.println("the result is : " + recursion.mul(4,1));
        System.out.println("the result is :  " + recursion.power(3,3,1));
     }
 }*/
 
-/* // unsortedarray .. insert , delete and search
+/*
 public class Main
-{
+{   // unsortedarray .. insert , delete and search
     public static void main(String[] args) {
         unorderarray a = new unorderarray(7);
         a.insert(12);
@@ -21,14 +47,15 @@ public class Main
         a.insert(4);
         a.insert(37);
         a.insert(10);
-
+        a.search(10);
         a.deletefirstmatch(12);
         a.displaydata();
     }
 } */
- /*  // sortedarray .. insert and delete .. binary search
+
+/*
 public class Main
-{
+{    // sortedarray .. insert and delete .. binary search
     public static void main(String[]args)
     {
         sortedarray a = new sortedarray(10);
---
+++ mergesorting.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+public class mergesorting {
+    /*public static void merge(int[] arr, int start, int mid, int end) {
+        int length1 = mid - start + 1;
+        int length2 = end - mid;
+        int[] r1 = new int[length1];
+        int[] r2 = new int[length2];
+        for (int i = 0; i < r1.length; i++) {
+            r1[i] = arr[start + i];
+        }
+        for (int i = 0; i < r2.length; i++) {
+            r2[i] = arr[mid + 1 + i];
+        }
+        for (int i = 0; i < r2.length; i++)
+        {
+            r2[i] = arr[mid + 1 + i];
+        }
+
+    }*/
+
+}
